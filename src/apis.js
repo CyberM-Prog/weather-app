7932290894a786b01f5487563b57f1e0a21579e3
Martim <martim.pintao@gmail.com>
2022-07-11 18:15:01 +0100

    Create function that returns the weather info of a location

--- src/apis.js
+++ src/apis.js
@@ -1,6 +1,6 @@
 const key = "9b2ecba753a2d0b501b16be3452469ff";
 
-export async function convertLocation(location) {
+async function getCoordinates(location) {
     const response = await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${key}`,
         {mode: "cors"}
@@ -8,4 +8,14 @@ export async function convertLocation(location) {
     const data = await response.json();
     const coordinates = {lat: data[0].lat, lon: data[0].lon};
     return coordinates;
+}
+
+export async function getWeatherInfo(location) {
+    const coordinates = await getCoordinates(location);
+
+    const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}`
+    );
+    const data = await response.json();
+    return data;
 }
