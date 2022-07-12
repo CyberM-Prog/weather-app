013f1969c1b1015a3fcefbb5339923d77ed7bca7
Martim <martim.pintao@gmail.com>
2022-07-12 13:02:19 +0100

    Create form that lets users search for weather info based on input

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./index.css";
 import * as apis from "./apis";
+
+const locationInput = document.getElementById("locationinput");
+const searchBtn = document.querySelector(".search");
+
+searchBtn.addEventListener("click", async () => {
+    const data = await apis.getWeatherInfo(locationInput.value);
+    console.log(data);
+});
