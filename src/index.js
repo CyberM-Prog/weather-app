82cc99a179a0a1b5b14039d4ceac0bd89b402c2e
Martim <martim.pintao@gmail.com>
2022-07-12 16:28:16 +0100

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import * as apis from "./apis";
 const locationInput = document.getElementById("locationinput");
 const searchBtn = document.querySelector(".search");
 
-searchBtn.addEventListener("click", async () => {
-    const data = await apis.getWeatherInfo(locationInput.value);
-    console.log(data);
-});
+async function searchWeather() {
+    try {
+        const data = await apis.getWeatherInfo(locationInput.value);
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+searchBtn.addEventListener("click", searchWeather);
